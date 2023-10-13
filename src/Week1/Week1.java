62c82b5a3eaee330a259be260df17996484968cf
MuneefMumthas <22206529@bucks.ac.uk>
2023-10-13 12:26:18 +0100

    Added loop prompt for week1 Ex

--- src/Week1/Week1.java
+++ src/Week1/Week1.java
@@ -1,23 +1,31 @@
 package Week1;
 import helpers.*;
+import java.util.Scanner;
 
 public class Week1 
 {
     public static void main(String[] args)
     {
-        System.out.println(ConsoleColours.CONSOLE_CLEAR);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("==================================");
-        System.out.println("CO452 Programming Concepts 2022/23");
-        System.out.println("==================================");
-        System.out.println();
-        System.out.println("  Week 1 Java Basic Input/Output ");
-        System.out.println();
-        System.out.println(" by Nicholas Day and Derek Peacock");
-        System.out.println();
+        do {
+            System.out.println(ConsoleColours.CONSOLE_CLEAR);
 
-        String name = InputReader.getString("Enter your name > ");
-        System.out.println("Hello " + name);
+            System.out.println("==================================");
+            System.out.println("CO452 Programming Concepts 2023/24");
+            System.out.println("==================================");
+            System.out.println();
+            System.out.println("  Week 1 Java Basic Input/Output ");
+            System.out.println();
+            System.out.println(" by Nicholas Day and Derek Peacock");
+            System.out.println();
 
+            String name = InputReader.getString("Enter your name > ");
+            System.out.println("Hello " + name);
+
+            System.out.println("Type 'close' to exit or any other key to restart.");
+        } while (!scanner.nextLine().equalsIgnoreCase("close"));
+
+        scanner.close();
     } 
 }
