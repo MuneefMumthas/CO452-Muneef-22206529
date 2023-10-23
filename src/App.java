ddcf3ef1db529ee54f0d132a323adf47bd1e69d7
MuneefMumthas <22206529@bucks.ac.uk>
2023-10-23 22:52:42 +0100

    Added Do WHile loop to valid check the inputs

--- src/App.java
+++ src/App.java
@@ -18,52 +18,60 @@ public class App {
     }
     public static void Options()
     {
+        /// Do While loop to keep asking for an option until a valid one is chosen
+        boolean ValidInput = false;
+        while (!ValidInput) 
+        {
             /// Prompts to choose an option
             System.out.println("1. Add a new song to the list of songs");
             System.out.println("2. Remove a song from the list of songs");
             System.out.println("3. Print a list of all the songs stored");
             System.out.println("4. Print a list of songs over a given number of plays");
-            System.out.println("");
-            System.out.println("Type 'close' to exit or any other key to restart.");
+            System.out.println("5. Quit");
             System.out.println("");
             String input = InputReader.getString("Enter your choice > ");
 
-                        /// Switch statement to choose an option
+            /// Switch statement to choose and run an option
             switch (input) 
             {
                 case "1":
                     System.out.println("");
                     System.out.println("option 1 selected");
                     System.out.println("");
-                    Options();
+                    ValidInput = true;
+                    
                     break;
                 case "2":
                     System.out.println("");
                     System.out.println("option 2 selected");
                     System.out.println("");
-                    Options();
+                    ValidInput = true;
+                    
                     break;
                 case "3":
                     System.out.println("");
                     System.out.println("option 3 selected");
                     System.out.println("");
-                    Options();
+                    ValidInput = true;
+                    
                     break;
                 case "4":
                     System.out.println("");
                     System.out.println("option 4 selected");
                     System.out.println("");
-                    Options();
+                    ValidInput = true;
+                    
                     break;
-                case "close":
+                case "5":
                     System.out.println("");
-                    System.out.println("Thank you for Using");
                     System.out.println("Application Closed");
                     System.out.println("");
                     System.exit(0);
                 default:
-                    break;
+                    System.out.println("");
+                    System.out.println("Invalid input. Please try again.");
+                    System.out.println("");
             }
+        }
     }
-
 }
