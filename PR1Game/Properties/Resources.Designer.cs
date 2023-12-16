457604807b0a7dde96f39a379891330dcf992484
MuneefMumthas <22206529@bucks.ac.uk>
2023-12-16 15:01:21 +0000

    added the icon to the resources

--- PR1Game/Properties/Resources.Designer.cs
+++ PR1Game/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace PR1Game.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Custom_Logo_for_Aerial_Assault {
+            get {
+                object obj = ResourceManager.GetObject("Custom_Logo_for_Aerial_Assault", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Enemy_Image {
